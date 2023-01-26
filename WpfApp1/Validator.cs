fc3c12f280bf08004bf917d894a00b889c8e5187
thu <thu@DESKTOP-EDV6754>
2023-01-26 23:03:56 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/Validator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Validator
+    {
+        public static string Passvalid(string pass)
+        {
+            if (pass.Length >= 7)
+            {
+                return null;
+            }
+            return "error";
+        }
+    }
+}
