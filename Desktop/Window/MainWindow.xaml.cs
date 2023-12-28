a0f96fe556f6380c9104edea82694c9495e28984
ShirmankinNI <ShirmankinNI@mail.ru>
2023-12-28 03:16:06 +0300

    Lr8

--- Desktop/Window/MainWindow.xaml.cs
+++ Desktop/Window/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media;
-using Desktop.Model;
-using Desktop.Repository;
-using Desktop.View;
+﻿using Desktop.View;
 
 namespace Desktop.Window
 {
